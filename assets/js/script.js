af09ff3904b1d7c14c06f21d7fe99f13e224a514
vomkaraxioned <omkarv@axioned.com>
2022-09-24 15:16:17 +0530

    changes added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,24 @@ const posts = document.querySelector(".posts");
 let fetchedData;
 let initial = 0;
 let limit = 10;
+let err;
 
 addBtn.addEventListener("click", addData);
 
 function addData() {
-    for (initial; initial < limit; initial++) {
-        let index = initial + 1;
-        posts.innerHTML += "<li class=\"post\"><h2>" + index + "." + fetchedData[initial].title + "</h2><p>" + fetchedData[initial].body + "</p></li>";
-        if (initial == fetchedData.length - 1) {
-            addBtn.style.display = "none";
+    if(err != ""){
+        posts.innerHTML += "<li class=\"post\"><h2 class=\"error\">" + err+ "</h2></li>";
+    }else{
+        for (initial; initial < limit; initial++) {
+            let index = initial + 1;
+            posts.innerHTML += "<li class=\"post\"><h2>" + index + "." + fetchedData[initial].title + "</h2><p>" + fetchedData[initial].body + "</p></li>";
+            if (initial == fetchedData.length - 1) {
+                addBtn.style.display = "none";
+            }
         }
+        limit += 10;
     }
-    limit += 10;
+
 }
 
 let xmlObj = new XMLHttpRequest();
@@ -26,9 +32,12 @@ xmlObj.onreadystatechange = function() {
     try {
         if (this.readyState == 4 && this.status == 200) {
             fetchedData = JSON.parse(this.responseText);
+            err = "";
+        }else {
+            throw "error occured ,error code:"+this.status;
         }
     } catch (e) {
-        alert(e);
+        err = e;
     }
 };
 xmlObj.open("GET", url);
